8d8fe7ff110273fe3a1d29efa457d6d2ae1f0900
halil.agin <halil.agin@gmail.com>
2016-08-04 20:45:58 +0100

    refactoring

--- varang.d.ts
+++ varang.d.ts
@@ -1,3 +1,2 @@
-/// <reference path="node_modules/angular2/typings/browser.d.ts" />
 export * from './components/VarGrid';
 export * from './components/core';
--- varang.js
+++ varang.js
@@ -2,7 +2,6 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-///<reference path="node_modules/angular2/typings/browser.d.ts"/>
 __export(require('./components/VarGrid'));
 __export(require('./components/core'));
 //# sourceMappingURL=varang.js.map
